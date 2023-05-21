ad53d7715b2167652b59f6300ebdba65c682fcc7
dannyl1u <dannyjialiliu@gmail.com>
2023-05-20 21:30:35 -0700

    ratio endpoint

--- app.py
+++ app.py
@@ -62,5 +62,51 @@ def get_number():
     print(total_score/count)
     return jsonify(total_score/count)
 
+
+@app.route('/ratio')
+def get_ratio():
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    
+
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey = DEVELOPER_KEY)
+    
+    pattern = r"v=([^&]+)"
+    match = re.search(pattern, request.args.get('video_id'))
+    if match:
+        video_id = match.group(1)
+    else:
+        print("Video ID not found.")
+
+    # video_id = request.args.get('video_id')
+
+    comment_request = youtube.commentThreads().list(
+        part="id,snippet",
+        videoId = video_id
+    )
+    response = comment_request.execute()
+    print(response)
+
+    total_score = 0
+    count = 0
+    total_positive = 0
+    total_negative = 0
+    
+    for item in response['items']:
+        text_display = item['snippet']['topLevelComment']['snippet']['textDisplay']
+        count+=1
+        total_score += TextBlob(text_display).sentiment.polarity
+        if TextBlob(text_display).sentiment.polarity >= 0:
+            total_positive += 1
+        else:
+            total_negative += 1
+
+    response = jsonify(positive=total_positive, negative=total_negative)     
+    return response
+
+
 if __name__ == '__main__':
     app.run()
